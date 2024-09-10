7d70c228fbea8480a63342efee9183b8f5c0e044
muhammaddanishirfan <danishanwer2008@gmail.com>
2024-09-10 16:57:55 +0500

    Add files via upload

---
+++ Assignment 2/app.js
@@ -0,0 +1,95 @@
+
+// // chap 6-9 q1
+
+// var number= +prompt("Enter Any Number")
+// var prein= ++number
+// document.write("The value of ++a is"+prein)
+
+// var number= +prompt("Enter Any Number")
+// var postin= number++
+// document.write("The value of a++ is"+postin)
+
+// var number= +prompt("Enter Any Number")
+// var predec= --number
+// document.write("The value of --a is"+ predec)
+
+// var number= +prompt("Enter Any Number")
+// var postdec= number++
+// document.write("The value of a-- is"+postdec)
+
+// // chap 6-9 q2
+
+// var a=2
+// var b=1
+
+// var result= --a - --b + ++b + b--
+
+// document.write(result)
+
+// document.write("--a=1\n --a - --b= \n --a - --b + ++b=4 \n --a - --b + ++b + b--= 3")
+
+// // chap 6-9 q3
+
+// var username = +prompt("Enter Your Name")
+// alert("Wellcome"+username)
+
+// chap 6-9 q5
+
+// a)
+
+// var subject1 = prompt("Enter any subject")
+// var subject2 = prompt("Enter any subject")
+// var subject3 = prompt("Enter any subject")
+
+// // b)
+
+// var totalmarks = 100
+
+// // c-d)
+
+// var subject1marks = +prompt("Enter marks of "+subject1)
+// var subject2marks = +prompt("Enter marks of "+subject2)
+// var subject3marks = +prompt("Enter marks of "+subject3)
+
+// // e)
+
+// document.write(`${subject1} : ${totalmarks} ${subject1marks} ${subject1marks/totalmarks*100}% \n`)
+// document.write(`${subject2} : ${totalmarks} ${subject2marks} ${subject2marks/totalmarks*100}% \n`)
+// document.write(`${subject3} : ${totalmarks} ${subject3marks} ${subject3marks/totalmarks*100}% \n`)
+
+
+
+
+
+
+
+// // chap 5 q1
+
+// var num1= +prompt("Enter A number");
+
+// var num2= +prompt("Enter Another number");
+
+// var sum = num1 + num2;
+
+// document.write("Sum of",num1,"and",num2,"is", sum);
+
+
+
+
+// // chap 5 q2
+
+// var sub = num1 - num2;
+
+// document.write("sub of",num1,"and",num2,"is", sum);
+
+// var mul = num1 * num2;
+
+// document.write("mul of",num1,"and",num2,"is", sum);
+
+// var div = num1 / num2;
+
+// document.write("div of",num1,"and",num2,"is", sum);
+
+// var modulus = num1 % num2;
+
+// document.write("mod of",num1,"and",num2,"is", sum);
